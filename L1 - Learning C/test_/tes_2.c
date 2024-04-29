fd09f618de9cf132d9645dd27632ccbe81c5c9d3
xiang-and-maple <cst2309152@gmail.com>
2024-04-29 19:18:18 +0800

    homework，，， getchar to delete the \n

--- L1 - Learning C/test_/tes_2.c
+++ L1 - Learning C/test_/tes_2.c
@@ -4,47 +4,64 @@
 
 int main()
 {
+	printf("This tool can help you justifity the letter you input is vowel or consonant.\n");
+	printf("you can input lower or upper letter.\nif you input the other character the test is over\n");
+	printf("Now");
 	while (1)
 	{
+		
+		// creat the letter list
 		int bo = 0;
-		char let;
+		char let = "a";
 		char lary[27] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0' };
 
-		printf("letter: ");
-		scanf("%c", &let);
+		// input the letter
+		printf(" please input your letter: ");
+		scanf("%c",&let);
+		getchar();
 		char llet = tolower(let);
+		
+		// find the letter
 		for (int i = 0; i < 26; i++)
 		{
 			if (llet == lary[i])
 			{
 				bo = 1;
-			}
-
-		}
-		switch (bo)
-		{
-		case 1:
-			switch (llet)
-			{
-			case'a':
-			case'e':
-			case'i':
-			case'o':
-			case'u':
-				printf("the letter %c is vowel\n", llet);
-				break;
-			default:
-				printf("the letter %c is conponent\n", llet);
 				break;
 			}
 
-		
-		default:
-			break;
-			break;
 		}
+
+		////*Switch way*/
+		//// justify the input whether is letter or not
+		//switch (bo)
+		//{
+		//case 1:
+		//	switch (llet)
+		//	{
+		//	case'a':
+		//	case'e':
+		//	case'i':
+		//	case'o':
+		//	case'u':
+		//		printf("the letter %c is vowel\n", llet);
+		//		continue;
+		//	default:
+		//		printf("the letter %c is conponent\n", llet);
+		//		continue;
+		//	}
+		//	
+
+		//
+		//default:
+		//	break;
+		//}
+		//printf("your input is ineffective,the test over");
+		//break;
 		
-		/*if (bo == 1)
+
+		// if way
+		if (bo == 1)
 		{
 			if (llet == 'a' || llet == 'e' || llet == 'i' || llet == 'o' || llet == 'u')
 			{
@@ -55,10 +72,10 @@ int main()
 				printf("the letter %c is conponent\n", llet);
 			}
 		}
-		else if (bo == 0)
+		else
 		{
 			printf("your input is effective, the test over\n");
 			break;
-		}*/
+		}
 	}
 }
